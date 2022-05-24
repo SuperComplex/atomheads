e74d993c2d6a92e3e5f4848f8398431557b1e216
Oleh Pshyk <opshyk@apple.com>
2022-05-24 14:29:30 +0300

    top-section

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/atom heads/i);
+  const linkElement = screen.getByText(/The FREE, infinitely Inclusive, & Carbon Neutral NFT/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
+// import { useState, useEffect } from "react";
 import Header from 'components/Header/Header';
+import TopSection from 'components/TopSection/TopSection';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <div className="top-section">
-        AtomHeads
-      </div>
+      <TopSection/>
     </div>
   );
 }
