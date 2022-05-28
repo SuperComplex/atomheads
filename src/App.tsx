20483ee4d4b0e9a53fa2608df2f897292303895b
Liuda Muzychka <liudmyla.soroka@gmail.com>
2022-05-29 02:55:37 +0300

    third section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import Header from 'components/Header/Header';
 import TopSection from 'components/TopSection/TopSection';
 import SecondSection from 'components/SecondSection/SecondSection';
+import ThirdSection from 'components/ThirdSection/ThirdSection';
 import FourthSection from 'components/FourthSection/FourthSection';
 import FifthSection from 'components/FifthSection/FifthSection';
 import './App.scss';
@@ -12,6 +13,7 @@ function App() {
       <Header/>
       <TopSection/>
       <SecondSection/>
+      <ThirdSection/>
       <FourthSection/>
       <FifthSection/>
     </div>
