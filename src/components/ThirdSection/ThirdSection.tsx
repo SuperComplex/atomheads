c4b44e4b8e4c8c057846b2a2dca6796ebede447c
Oleh Pshyk <opshyk@apple.com>
2022-05-29 04:14:51 +0300

    alignments

--- src/components/ThirdSection/ThirdSection.tsx
+++ src/components/ThirdSection/ThirdSection.tsx
@@ -1,26 +1,30 @@
 import './ThirdSection.scss';
 
 function ThirdSection() {
-    return (
-        <section className="third-section">
-          <div className="third-section-container wrapper">
-              <div className="left-container">
-                  <div className="img1-container">
-                      <img className='img1' src="/img/Building.svg" alt="Building"/>
-                      </div>
-                      </div>
-                      </div>  
-                      <div className="right-container">
-                      <img className='img2' src="/img/bonding-building.svg" alt="bonding-building"/> 
-                      <p className="t1">
-                      In Web3, AtomHeads were made to bond & build community amongst all member. 
-          </p>
-        <p className="t2">
-        The infinitely free CC0 NFT Avatar grows the Web3 Community the way it was made to be originally - For everyone, by everyone.
-        </p>
-                      </div> 
-        </section>
-    )
+  return (
+    <section className="third-section">
+      <div className="third-section-container wrapper">
+        <div className="left-container">
+          <div className="img1-container">
+            <img className='img1' src="/img/Building.svg" alt="Building"/>
+          </div>
+        </div> 
+        <div className="right-container">
+          <div className="img2-container">
+            <img className='img2' src="/img/bonding-building.svg" alt="bonding-building"/> 
+          </div>
+          <div className="text">
+            <p>
+              In Web3, AtomHeads were made to bond & build community amongst all member. 
+            </p>
+            <p>
+              The infinitely free CC0 NFT Avatar grows the Web3 Community the way it was made to be originally - For everyone, by everyone.
+            </p>
+          </div>
+        </div>
+      </div> 
+    </section>
+  )
 }
 
 export default ThirdSection;
